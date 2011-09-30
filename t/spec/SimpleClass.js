0d8368d7ed55cda9a1ce2913f87606a1c575c164
Johnathan Leppert <Johnathan.Leppert@gmail.com>
2011-09-29 18:15:11 -0700

    Changes to support complex mixed classes and more tests

--- t/spec/SimpleClass.js
+++ t/spec/SimpleClass.js
@@ -1,7 +1,36 @@
-window.SimpleClass = 'test';
+(function() {
+    var randomString = function() {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var string_length = 8;
+        var randomString = '';
+        
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomString += chars.substring(rnum,rnum+1);
+        }
+
+        return randomString;
+    };
+    window.oldClass = randomString();
+    window.SimpleClass = oldClass;
+})();
+
 describe('SimpleClass', function() {
     var global = window;
     var SimpleClass;
+
+    var randomString = function() {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var string_length = 8;
+        var randomString = '';
+        
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomString += chars.substring(rnum,rnum+1);
+        }
+
+        return randomString;
+    };
 
     describe('Base Methods', function() {
         it('should provide object into global namespace', function() {
@@ -17,9 +46,9 @@ describe('SimpleClass', function() {
         it('should provide a _noConflict method', function() {
             expect(typeof(global.SimpleClass._noConflict)).toEqual('function');
         });
-        it('should remove the global object when noConflict is called', function() {
+        it('should remove the global object when noConflict is called and return itself', function() {
             SimpleClass = global.SimpleClass._noConflict();
-            expect(global.SimpleClass).toEqual('test');
+            expect(global.SimpleClass).toEqual(oldClass);
             expect(SimpleClass._extend).toBeDefined();
         });
     });
@@ -33,7 +62,90 @@ describe('SimpleClass', function() {
 
             expect(myObj.prototype.name).toEqual('obj');
             expect(typeof(myObj.prototype.something)).toEqual('function');
+
+            var instance = new myObj();
+
+            expect(instance.name).toEqual('obj');
+            expect(typeof(instance.something)).toEqual('function');
         });
+
+        it('should support static properties', function() {
+            var myObj = SimpleClass._extend({
+                _static: {
+                    something: 'else',
+                    func: function() { }
+                }
+            });
+
+            expect(myObj.something).toEqual('else');
+            expect(typeof(myObj.func)).toEqual('function');
+
+            var instance = new myObj();
+
+            expect(instance.something).not.toBeDefined();
+            expect(instance.func).not.toBeDefined();
+        });
+
+        it('should support instance properties', function() {
+            var myObj = SimpleClass._extend({
+                _instance: {
+                    something: 'else',
+                    func: function() { }
+                }
+            });
+
+            var instance = new myObj();
+
+            expect(instance.something).toEqual('else');
+            expect(typeof(instance.func)).toEqual('function');
+        });
+
+        it('should support simple mixin objects on the prototype', function() {
+            var mixin = {
+                one: 'two',
+                three: function() { }
+            };
+            
+            var myObj = SimpleClass._extend({
+                _instance: {
+                    something: 'else',
+                    func: function() { }
+                },
+                _mix: [mixin]
+            });
+
+            expect(myObj.prototype.one).toEqual('two');
+            expect(typeof(myObj.prototype.three)).toEqual('function');
+        });
+
+        it('should support complex mixins', function() {
+            var myMixin = SimpleClass._extend({
+                _instance: {
+                    something: 'else'
+                },
+                _static: {
+                    mymethod: function() { }
+                },
+                name: 'bob'
+            });
+
+            var myObj = SimpleClass._extend({
+                _instance: {
+                    one: 'two'
+                },
+                _static: {
+                    something: 'here'
+                },
+                _mix: [myMixin],
+                color: 'blue'
+            });
+
+            expect(myObj.mymethod).toBeDefined();
+        });
+
+
+
+
 
     });
 
